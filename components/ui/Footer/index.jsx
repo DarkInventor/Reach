9f081bd29b9de8af28783486db2b3ac60fb7c68b
DarkInventor <ktmehta25@gmail.com>
2023-08-08 20:42:24 -0400

    update

--- components/ui/Footer/index.jsx
+++ components/ui/Footer/index.jsx
@@ -8,6 +8,11 @@ const navigation = [
     { name: "FAQs", href: "#faqs" },
 ];
 
+const handleTwitterConnect = () => {
+    const twitterUrl = 'https://twitter.com/kathanmehtaa';
+    window.location.href = twitterUrl;
+  };
+
 const Footer = () => (
     <footer className="pt-0 sm:pt-0 text-center">
         <div className="custom-screen text-gray-600">
@@ -15,11 +20,19 @@ const Footer = () => (
                 <div className="space-y-4 text-center">
                     <Link href="/" className="inline-block">
                         <Brand />
+   
                     </Link>
+                    
                     <h2 className="text-gray-800 text-lg font-semibold sm:text-2xl">Websites Traffic Generation</h2>
                     <p className="max-w-md">
                         We use the best strategies for companies worldwide to improve traffic on their sites.
                     </p>
+                    <button type="button" onClick={handleTwitterConnect} class="text-white bg-[#1da1f2] hover:bg-[#1da1f2]/90 focus:ring-4 focus:outline-none focus:ring-[#1da1f2]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#1da1f2]/55 mr-2 mb-2">
+  <svg class="w-4 h-4 mr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 17">
+    <path fill-rule="evenodd" d="M20 1.892a8.178 8.178 0 0 1-2.355.635 4.074 4.074 0 0 0 1.8-2.235 8.344 8.344 0 0 1-2.605.98A4.13 4.13 0 0 0 13.85 0a4.068 4.068 0 0 0-4.1 4.038 4 4 0 0 0 .105.919A11.705 11.705 0 0 1 1.4.734a4.006 4.006 0 0 0 1.268 5.392 4.165 4.165 0 0 1-1.859-.5v.05A4.057 4.057 0 0 0 4.1 9.635a4.19 4.19 0 0 1-1.856.07 4.108 4.108 0 0 0 3.831 2.807A8.36 8.36 0 0 1 0 14.184 11.732 11.732 0 0 0 6.291 16 11.502 11.502 0 0 0 17.964 4.5c0-.177 0-.35-.012-.523A8.143 8.143 0 0 0 20 1.892Z" clip-rule="evenodd"/>
+  </svg>
+  Connect on Twitter
+</button>
                     {/* <div className="pt-2 mt-8 flex items-center gap-x-6 text-gray-400 justify-center">
                         <a href="https://www.facebook.com/kathan.mehta.315/" target="_blank" rel="noreferrer" aria-label="Social media">
                             <svg className="w-6 h-6 hover:text-gray-500 duration-150" fill="none" viewBox="0 0 48 48"><g clipPath="url(#a)"><path fill="currentColor" d="M48 24C48 10.745 37.255 0 24 0S0 10.745 0 24c0 11.979 8.776 21.908 20.25 23.708v-16.77h-6.094V24h6.094v-5.288c0-6.014 3.583-9.337 9.065-9.337 2.625 0 5.372.469 5.372.469v5.906h-3.026c-2.981 0-3.911 1.85-3.911 3.75V24h6.656l-1.064 6.938H27.75v16.77C39.224 45.908 48 35.978 48 24z" /></g><defs><clipPath id="a"><path fill="#fff" d="M0 0h48v48H0z" /></clipPath></defs></svg>
