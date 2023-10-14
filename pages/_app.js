29f1d22f0b8db48ee7ab8341037862526e062709
DarkInventor <ktmehta25@gmail.com>
2023-10-15 01:53:02 +0530

    updated with new badge

--- pages/_app.js
+++ pages/_app.js
@@ -17,12 +17,13 @@ export default function App({ Component, pageProps }) {
         `,
       }}
     />
-        <title>Reach</title>
+        <title>Reachactory</title>
         <meta
           name='description'
           content='The ultimate way to get more website traffic and grow your online business.'
         />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <meta property="og:image" content="/reach.png" />
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <Component {...pageProps} />
